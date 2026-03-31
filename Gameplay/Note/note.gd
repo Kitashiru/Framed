bfc9340ab33b30e4f46182100555b78f7aa039ed
Kitashiru <kbrunke31@gmail.com>
2026-03-31 12:56:58 -0400

    Fixed Note Bug

--- Gameplay/Note/note.gd
+++ Gameplay/Note/note.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var NoteLayer = $CanvasLayer
-@onready var collisionshape = $Area2D/CollisionShape2D
+@onready var collisionshape = $Area2D/CollisionPolygon2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,14 +9,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("left_click"):
-		if NoteLayer.visible == true:
-			NoteLayer.hide()
-			await get_tree().create_timer(0.5).timeout
-			collisionshape.disabled = false
+	pass
 
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("left_click"):
 		NoteLayer.show()
 		collisionshape.disabled = true
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if NoteLayer.visible:
+			NoteLayer.hide()
+			await get_tree().create_timer(0.2).timeout
+			collisionshape.disabled = false
